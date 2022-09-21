ee26b27b6fcb842de431dcb8fa1a9d8a2a57a934
Julia <xx_julia_xx@mail.ru>
2022-09-22 01:01:06 +0300

    Добавила папки с ДЗ

---
+++ Z4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите три числа: ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int с = int.Parse(Console.ReadLine());
+int max = a;
+if (max<b);
+max = b;
+if(max<с);
+max = с;
+Console.Write("Максимальное число: " + max);
---
+++ Z8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число больше 1: ");
+
+int a = int.Parse(Console.ReadLine());
+int b = 2;
+if (a <=1)
+{
+     Console.WriteLine("Некорректное число");
+}
+while (b <= a)
+{
+    Console.WriteLine(b);
+    b = b + 2;
+}
