9756dd630f29a7cb7c0019b549e1d8439d1e04de
Julia <xx_julia_xx@mail.ru>
2022-09-25 13:35:53 +0300

    Добавила задачу 10 второй семинар

---
+++ Z10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+
+int a = int.Parse(Console.ReadLine());
+
+int b = a / 10;
+int c = a / 100 * 10;
+int d = b - c;
+Console.WriteLine(d);
