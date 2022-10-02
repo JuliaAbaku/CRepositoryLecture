8f55e6d196eef72b51b4b74886cfdd6e9b723ba8
Julia <xx_julia_xx@mail.ru>
2022-10-02 16:15:06 +0300

    Добавила ДЗ 25,27,29

---
+++ Z27/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Console.Clear();
+
+// Console.WriteLine("Введите число");
+// int numb = int.Parse(Console.ReadLine());
+
+// string numb1 = numb.ToString();
+
+// int sum = 0;
+// for (int i = 0; i <= numb1.Length-1; i++)
+// {
+//    sum = sum + Convert.ToInt32(numb1[i]-'0'); // сама думала что  sum = sum + numb1[i], но не получается.
+//                                             // Потом поняла, что надо конвертировать numb1[i] в число int
+//                                             // Все равно не получалось. В интернете нашла что надо -'0'
+//                                             // Но я не понимаю, что это. Дайте, пожалуйста, пояснение
+// }
+// Console.WriteLine(sum);
+
+
+
+
+Console.Clear();
+
+Console.WriteLine("Введите число");
+int numb = int.Parse(Console.ReadLine());
+int sum = 0;
+while (numb>0)
+{
+    sum = sum + (numb%10);
+    numb = numb/10;
+}
+Console.WriteLine(sum);
---
+++ Z29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+Console.WriteLine("Введите число элементов в массиве");
+int N = int.Parse(Console.ReadLine());
+
+int[] Random01(int[] array_local)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array_local.Length; i++)
+    {
+        array_local[i] = rnd.Next(0, 11); // тут 11 - это числа которые можно использовать. 
+                                          // Получается массив будет состоять из чисел от 0 до 10
+                                          
+    }
+    return array_local;
+}
+
+int[] array = new int[N];
+array = Random01(array);
+
+Console.Write(String.Join(",",array));
